fd4d8b884264c7c900af7e8f4622c5c4541481f3
Owais <33609590+OwaisAD@users.noreply.github.com>
2023-04-25 01:21:09 +0200

    added settings menu

--- HandIn/Assets/Scripts/MenuController.cs
+++ HandIn/Assets/Scripts/MenuController.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 0.5f;
+
+    [Header("Gameplay Settings")]
+    [SerializeField] private TMP_Text controllerSensTextValue = null;
+    [SerializeField] private Slider controllerSensSlider = null;
+    [SerializeField] private int defaultSens = 5;
+    public int mainControllerSens = 5;
+
+    [Header("Toggle Settings")]
+    [SerializeField] private Toggle invertYToggle = null;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
@@ -33,5 +51,67 @@ public class MenuController : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("mastervolume", AudioListener.volume);
+        // Show Prompt
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void SetControllerSens(float sensitivity)
+    {
+        mainControllerSens = Mathf.RoundToInt(sensitivity);
+        controllerSensTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        if(invertYToggle.isOn)
+        {
+            PlayerPrefs.SetInt("masterInvertY", 1);
+            // do the inverting here
+        }
+        else 
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0);
+            // ! do the inverting here
+        }
+        PlayerPrefs.SetFloat("masterSens", mainControllerSens);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio") 
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+
+        if(MenuType == "Gameplay")
+        {
+            controllerSensTextValue.text = defaultSens.ToString("0");
+            controllerSensSlider.value = defaultSens;
+            mainControllerSens = defaultSens;
+            invertYToggle.isOn = false;
+            GameplayApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox() 
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
