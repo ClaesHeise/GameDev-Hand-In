302b0d0a9ce3a95d63c27d94d61a4a5cae52dceb
ClaesHeise <43722036+ClaesHeise@users.noreply.github.com>
2023-05-06 19:00:00 +0200

    Tropic island completed
    - Tropic island kan man gå på
    - De sidste puzzles er lavet, så der er 3 i alt
    - Det nye input system er nu inkorporeret i alle spillerens inputs

--- HandIn/Assets/Prefabs/Player/Jump.cs
+++ HandIn/Assets/Prefabs/Player/Jump.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jump : MonoBehaviour
 {
@@ -11,10 +12,18 @@ public class Jump : MonoBehaviour
     private Rigidbody _rbody;
     private Vector3 _jumpInput;
     bool isGrounded;
+
+    private MovementInput moveInp;
+
+    void Awake()
+    {
+        moveInp = new MovementInput();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        isGrounded = true;
+        isGrounded = false;
         _rbody = GetComponent<Rigidbody>();
         if(_rbody is null){
             Debug.Log("Rigidbody is NULL!");
@@ -24,16 +33,34 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _rbody.AddForce(Physics.gravity * (_gravityScale - 1) * _rbody.mass);
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
-            _rbody.AddForce(Vector3.up * _jumpHeight, ForceMode.Impulse);
-            isGrounded = false;
+        if(isGrounded == false){
+            _rbody.AddForce(Physics.gravity * (_gravityScale - 1) * _rbody.mass);
         }
+        var keyboard = Keyboard.current;
+        if(keyboard == null){
+            return;
+        }
+        if(isGrounded && moveInp.Player_Move.Jump.triggered){
+            _rbody.AddForce(Vector3.up * _jumpHeight, ForceMode.Impulse);
+        }
+
     }
 
-    private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.tag == "Map"){
-            isGrounded = true;
-        }    
+    private void FixedUpdate() {
+        isGrounded = false;    
+    }
+
+    void OnEnable() {
+        moveInp.Player_Move.Enable();
+    }
+
+    void OnDisable() {
+        moveInp.Player_Move.Disable();
+    }
+
+    private void OnCollisionStay(Collision other) {
+        if(other.contacts[0].normal.y >= 0.6f){
+        isGrounded = true;
+    }
     }
 }
