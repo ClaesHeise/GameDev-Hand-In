3fef1b9c9f4cc61b34e9f88aaa29c64451b69cfc
ClaesHeise <43722036+ClaesHeise@users.noreply.github.com>
2023-06-05 23:15:34 +0200

    added enemy animations

--- HandIn/Assets/Scripts/EnemyMovement.cs
+++ HandIn/Assets/Scripts/EnemyMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+
+    Animator animator;
+
     private NavMeshAgent m_navMeshAgent;
 
     [SerializeField] private float m_wanderRange = 15.0f;
@@ -13,30 +16,40 @@ public class EnemyMovement : MonoBehaviour
     private void Awake()
     {
         m_navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = this.GetComponent<Animator>();
     }
 
     public void MoveTo(Vector3 targetPosition) => m_navMeshAgent.SetDestination(targetPosition);
 
     public void Wander()
     {
-        /*if (m_navMeshAgent.isStopped)
+        if (m_navMeshAgent.isStopped)
         {
             return;
-        }*/
+        }
 
         GameObject player = GameObject.FindWithTag("Player");
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
         float distanceToTargetPosition = Vector3.Distance(transform.position, m_targetPosition);
-        if (distanceToPlayer > 15 && distanceToTargetPosition == 0)
+        if (distanceToPlayer > 50)// && distanceToTargetPosition == 0)
         {
-            StartCoroutine(Idle(Random.Range(1F, 4F)));
-            m_targetPosition = GetRandomPosition();
+            animator.SetInteger("state", 0);
+            m_targetPosition = transform.position;
+            // m_targetPosition = GetRandomPosition();
+            // StartCoroutine(Idle(Random.Range(1F, 4F)));
         }
-        else
-        {
+        // else if(distanceToTargetPosition < GetStoppingDistance()){
+
+        //     m_targetPosition = GetRandomPosition();
+        //     StartCoroutine(Idle(Random.Range(1F, 4F)));
+        // }
+        else if(distanceToPlayer < 50 && distanceToPlayer > 5) {
+            animator.SetInteger("state", 1);
             m_targetPosition = player.transform.position;
         }
-
+        else if(distanceToPlayer < 5){
+            print("Dealt 20 damage");
+        }
         MoveTo(m_targetPosition);
     }
 
