213a89de1d3295078b00925b69002edf1e30aa44
Legolai <Nico360f@gmail.com>
2023-06-06 09:20:48 +0200

    update enemy script

--- HandIn/Assets/Scripts/EnemyMovement.cs
+++ HandIn/Assets/Scripts/EnemyMovement.cs
@@ -6,67 +6,72 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
 
-    Animator animator;
+  Animator animator;
 
-    private NavMeshAgent m_navMeshAgent;
+  private NavMeshAgent m_navMeshAgent;
 
-    [SerializeField] private float m_wanderRange = 15.0f;
-    [SerializeField] private Vector3 m_targetPosition;
+  [SerializeField] private float chaseSpeed = 15.0f;
+  [SerializeField] private float m_wanderRange = 15.0f;
 
-    private void Awake()
+  [SerializeField] private Vector3 m_targetPosition;
+
+  private float initialSpeed;
+
+  private void Awake()
+  {
+    m_navMeshAgent = GetComponent<NavMeshAgent>();
+    animator = this.GetComponent<Animator>();
+    m_targetPosition = GetRandomPosition();
+    initialSpeed = m_navMeshAgent.speed;
+  }
+
+  public void MoveTo(Vector3 targetPosition) => m_navMeshAgent.SetDestination(targetPosition);
+
+  public void Wander()
+  {
+    m_navMeshAgent.speed = initialSpeed;
+    if (m_navMeshAgent.isStopped)
     {
-        m_navMeshAgent = GetComponent<NavMeshAgent>();
-        animator = this.GetComponent<Animator>();
+      animator.Play("Idle");
+      return;
     }
 
-    public void MoveTo(Vector3 targetPosition) => m_navMeshAgent.SetDestination(targetPosition);
-
-    public void Wander()
+    float distanceToTargetPosition = Vector3.Distance(transform.position, m_targetPosition);
+    if (distanceToTargetPosition < GetStoppingDistance())
     {
-        if (m_navMeshAgent.isStopped)
-        {
-            return;
-        }
-
-        GameObject player = GameObject.FindWithTag("Player");
-        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-        float distanceToTargetPosition = Vector3.Distance(transform.position, m_targetPosition);
-        if (distanceToPlayer > 50)// && distanceToTargetPosition == 0)
-        {
-            animator.SetInteger("state", 0);
-            m_targetPosition = transform.position;
-            // m_targetPosition = GetRandomPosition();
-            // StartCoroutine(Idle(Random.Range(1F, 4F)));
-        }
-        // else if(distanceToTargetPosition < GetStoppingDistance()){
-
-        //     m_targetPosition = GetRandomPosition();
-        //     StartCoroutine(Idle(Random.Range(1F, 4F)));
-        // }
-        else if(distanceToPlayer < 50 && distanceToPlayer > 5) {
-            animator.SetInteger("state", 1);
-            m_targetPosition = player.transform.position;
-        }
-        else if(distanceToPlayer < 5){
-            print("Dealt 20 damage");
-        }
-        MoveTo(m_targetPosition);
+      m_targetPosition = GetRandomPosition();
+      StartCoroutine(Idle(Random.Range(1F, 4F)));
     }
 
-    private Vector3 GetRandomPosition()
+    animator.Play("Walking");
+    MoveTo(m_targetPosition);
+  }
+
+  public void Chase(Vector3 targetPosition)
+  {
+    if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Running"))
     {
-        Vector3 randomPosition = Random.insideUnitSphere * (m_wanderRange * 1.25f) + transform.position;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomPosition, out hit, (m_wanderRange * 1.25f), NavMesh.AllAreas);
-        return hit.position;
+      animator.Play("Base Layer.Running");
     }
 
-    public float GetStoppingDistance() => m_navMeshAgent.stoppingDistance;
+    m_navMeshAgent.speed = chaseSpeed;
+    MoveTo(targetPosition);
+  }
 
-    private IEnumerator Idle(float idleTime)
-    {
-        m_navMeshAgent.isStopped = true;
-        yield return new WaitForSeconds(idleTime);
-        m_navMeshAgent.isStopped = false;
-    }
+  private Vector3 GetRandomPosition()
+  {
+    Vector3 randomPosition = Random.insideUnitSphere * (m_wanderRange * 1.25f) + transform.position;
+    NavMeshHit hit;
+    NavMesh.SamplePosition(randomPosition, out hit, (m_wanderRange * 1.25f), NavMesh.AllAreas);
+    return hit.position;
+  }
+
+  public float GetStoppingDistance() => m_navMeshAgent.stoppingDistance;
+
+  private IEnumerator Idle(float idleTime)
+  {
+    m_navMeshAgent.isStopped = true;
+    yield return new WaitForSeconds(idleTime);
+    m_navMeshAgent.isStopped = false;
+  }
 }
